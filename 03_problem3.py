ec97654d4352aca4153a752c3b1e715a3a2a9f07
uvpatel <uvpatel7271@gmail.com>
2024-09-08 09:43:40 -0700

    chapter _2-ps

--- 03_problem3.py
+++ 03_problem3.py
@@ -1,8 +1,2 @@
-# 3. Attempt problem 1 using while loop.
-#  Write a program to print multiplication table of a given number using for loop.
-
-number = int(input("Enter your number: "))
-i = 1
-for i in range(1,11):
-    print(f"{number}x{i} = {number*i}")
-    i += 1
+inut = input("Enter : ")
+print(type(inut))
